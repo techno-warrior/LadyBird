8c49b545e6fdf2ec2a78d9a761ec9b98057457fd
danieldunn <daniel.dunn@gmx.co.uk>
2015-05-12 23:08:25 +0100

    Stuff.iml

--- JavaStuff/src/JavaThings/Hello.java
+++ JavaStuff/src/JavaThings/Hello.java
@@ -1,8 +1,10 @@
+package JavaThings;
+
 /**
  * Created by Daniel on 12/05/2015.
  */
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Yes.");
+        System.out.println("hi.");
     }
 }
