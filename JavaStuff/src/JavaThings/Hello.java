0986a20cb52533f4f93230161a2ac2acc9e3d4da
techno-warrior <ryandmonte@gmail.com>
2015-05-12 23:21:40 +0100

    yes.

--- JavaStuff/src/JavaThings/Hello.java
+++ JavaStuff/src/JavaThings/Hello.java
@@ -4,7 +4,8 @@ package JavaThings;
  * Created by Daniel on 12/05/2015.
  */
 public class Hello {
+    MakeStar s = new MakeStar(10);
     public static void main(String[] args){
-        System.out.println("hi.");
+        System.out.println(s.getStars());
     }
 }
