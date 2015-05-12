6f968b00d62c5d2fc0d988f93fb50d9e081256e7
techno-warrior <ryandmonte@gmail.com>
2015-05-12 22:11:30 +0100

    testing files

--- src/src/HelloFriend.java
+++ src/src/HelloFriend.java
@@ -3,10 +3,33 @@
  */
 
 //This is a test class to see if we can see the same stuff.
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class HelloFriend {
+public class HelloFriend extends Application{
         public static void main(String[] args){
-            System.out.println("Hello Ryan");
-            //If you can see this delete this comment and say hello back.
+            launch(args);
         }
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Hello Daniel!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello Daniel!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }
