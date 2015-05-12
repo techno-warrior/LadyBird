b19decc921c6f47d92b4df359a8a1c35540975ff
danieldunn <daniel.dunn@gmx.co.uk>
2015-05-12 21:54:23 +0100

    Added some actual code.

---
+++ JavaSource/src/HelloFriend.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Daniel on 12/05/2015.
+ */
+
+//This is a test class to see if we can see the same stuff.
+
+public class HelloFriend {
+        public static void main(String[] args){
+            System.out.println("Hello Ryan");
+            //If you can see this delete this comment and say hello back.
+        }
+}
