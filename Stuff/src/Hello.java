ee2192b7d0a42249d93526499bc6a7a61c519aea
danieldunn <daniel.dunn@gmx.co.uk>
2015-05-12 22:55:57 +0100

    Yes.

---
+++ Stuff/src/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Daniel on 12/05/2015.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Yes.");
+    }
+}
--- src/src/ConsoleOutput.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-/**
- * Created by Daniel on 12/05/2015.
- */
-public class ConsoleOutput {
-}
